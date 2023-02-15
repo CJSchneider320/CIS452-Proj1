f8a7829e52fda55c56ed66ee5c62aa4e38108e2d
CJSchneider320 <cjschneider320@gmail.com>
2023-02-15 14:20:54 -0500

    init

---
+++ main.c
@@ -0,0 +1,10 @@
+/***************************************
+*
+*  CIS 452, Project 1
+*  Cameron Schneider, Kassidy Ricketson
+*
+***************************************/
+
+int main() {
+    return 1;
+}
